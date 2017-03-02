37e088b7b0514623d4149245615ec02208ba0ac9
Wojciech Kaczmarek <wojtekk@kofeina.net>
2017-03-02 19:04:44 +0100

    openqs optimization: n-1 -> last

--- openqs.go
+++ openqs.go
@@ -41,9 +41,9 @@ func (q *queue) Push(obj interface{}) {
 }
 
 func (q *queue) Pop() interface{} {
-	n := len(*q)
-	x := (*q)[n-1]
-	*q = (*q)[0 : n-1]
+	last := len(*q) - 1
+	x := (*q)[last]
+	*q = (*q)[0:last]
 	return x
 }
 
